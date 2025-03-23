51a7edb6c1e1d263a8cb24c2bcd10a401bfd22b5
gagadekar <ganeshvgadekar@gmail.com>
2025-03-23 20:37:33 +0530

    my 2nd branch fist commit

--- Script1.sh
+++ Script1.sh
@@ -2,7 +2,4 @@
 #This is my 1st shell script to print output
 echo "Hello All, Welcome to AWS/Devops Class"
 echo "Hello, How are you?"
-echo "Hello, good evening"
-echo "Hii"
-echo "hello"
-echo "Ganesh, how are you ? todays date is 23rd march 
+echo "Hello, Best of luck"
